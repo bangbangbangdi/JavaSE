571de328a693df9736de3257c0fcefb278233a47
bangdi_mac <19071916@qq.com>
2021-03-05 12:10:02 +0800

    first commit

--- netdemo/src/com/bangdi/client/Client.java
+++ netdemo/src/com/bangdi/client/Client.java
@@ -7,7 +7,10 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        //创建socket对象，开启实现io的虚拟接口
+        //需要制指定数据接收放的ip地址和端口号
         Socket client = new Socket("localhost", 10086);
+        //获取输出流对象，向服务器端发送数据
         OutputStream outputStream = client.getOutputStream();
         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
         dataOutputStream.writeUTF("Hello,bangdi");
--- netdemo/src/com/bangdi/server/Server.java
+++ netdemo/src/com/bangdi/server/Server.java
@@ -8,7 +8,9 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        //服务器端使用ServerSocket来开放本地的端口
         ServerSocket serverSocket = new ServerSocket(10086);
+        //接收client传输过来的数据，需要定义socket对象
         Socket socket = serverSocket.accept();
         InputStream inputStream = socket.getInputStream();
         DataInputStream dataInputStream = new DataInputStream(inputStream);
